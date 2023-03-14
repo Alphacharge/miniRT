f4e102136d9f76cf3a5a8fdf3f3c461e5b02184a
Alphacharge <rbetz@student.42heilbronn.de>
2023-03-14 16:24:11 +0100

    formula for cylinder surface without cap, before franziska optimization

--- src/objects/hit_cylinder.c
+++ src/objects/hit_cylinder.c
@@ -6,75 +6,111 @@
 /*   By: rbetz <rbetz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 14:22:10 by rbetz             #+#    #+#             */
-/*   Updated: 2023/03/11 14:49:06 by rbetz            ###   ########.fr       */
+/*   Updated: 2023/03/14 16:23:16 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-//this version shows only the circle part of the cylinder and only 1 of it
+// static void	copy_ray(t_ray *ray, t_ray *myray, t_obj *obj)
+// {
+// 	if (myray->closest_t > T_MIN && myray->closest_t < T_MAX && myray->closest_t < ray->closest_t)
+// 	ray->closest_object = obj;
+// 	ray->closest_t = myray->closest_t;
+// 	ray->normal = myray->normal;
+// }
 
+// bool	hit_cylinder(t_ray *ray, t_obj *obj)
+// {
+// 	t_ray	myray;
+// 	// t_ray	myray2;
+// 	t_obj	myobj;
+// 	t_vec	vec;
+
+// 	myray.origin = ray->origin;
+// 	myray.direction = ray->direction;
+// 	vec = add_vector(*obj->coord, factor_mult_vector(*obj->vector, obj->hei_fov / 2));
+// 	myobj.coord = &vec;
+// 	myobj.vector = obj->vector;
+// 	myobj.radius = obj->radius;
+// 	if (hit_plane(&myray, &myobj) && hit_sphere(&myray, &myobj))
+// 		copy_ray(ray, &myray, obj);
+// 	*myobj.coord = add_vector(*obj->coord, factor_mult_vector(*obj->vector, obj->hei_fov / -2));
+// 	if (hit_plane(&myray, &myobj) && hit_sphere(&myray, &myobj))
+// 		copy_ray(ray, &myray, obj);
+
+//     double a = scalar_vector(ray->direction, ray->direction);
+//     t_vec m = subtract_vector(ray->origin, *obj->coord);
+//     double b = 2 * scalar_vector(ray->direction, m);
+//     double c = scalar_vector(m, m) - obj->radius * obj->radius;
+//     double discriminant = b * b - 4 * a * c;
+//     if (discriminant < 0) {
+//         return false;  // no intersection
+//     }
+//     double t1 = (-b - sqrt(discriminant)) / (2 * a);
+//     double t2 = (-b + sqrt(discriminant)) / (2 * a);
+//     double t = t1 < t2 ? t1 : t2;
+//     t_vec intersection = add_vector(ray->origin, factor_mult_vector(ray->direction, t));
+//     if (intersection.z < obj->coord->z - obj->hei_fov / 2 ||
+//         intersection.z > obj->coord->z + obj->hei_fov / 2)
+//         return false;  // intersection is outside cylinder height
+// 	if (t > T_MIN && t < T_MAX && t < ray->closest_t)
+// 	{
+//     	ray->closest_object = obj;
+//     	ray->closest_t = t;
+//     	ray->normal = unit_vector(subtract_vector(intersection, *obj->coord));
+//     	return true;
+// 	}
+// 	return (false);
+
+// }
+
+
+//mit hannes
 bool	hit_cylinder(t_ray *ray, t_obj *obj)
 {
-	double	t;
-	double	dist;
-	double	div;
+	double	abc[3];
+	double	t[2];
+	double	x;
 	t_vec	inter;
-	t_vec	proj;
-	// Step 3: Calculate t
-	div = scalar_vector(ray->direction, *obj->vector);
-	if (div == 0)
-		return (false);
-	t = (scalar_vector(*obj->coord, *obj->vector) - scalar_vector(ray->origin, *obj->vector)) / div;
-	if (t < T_MIN || t > T_MAX)
-		return (false);
-	// Step 4: Calculate the coordinates of the intersection point
-	inter = add_vector(ray->origin, factor_mult_vector(ray->direction, t));
-	// Step 5: Check if the intersection point lies on the lateral surface of the cylinder
-	proj = add_vector(*obj->coord, factor_mult_vector(*obj->vector, scalar_vector(subtract_vector(inter, *obj->coord), *obj->vector)));
-	dist = sqrt(pow(inter.x - proj.x, 2) + pow(inter.y - proj.y, 2) + pow(inter.z - proj.z, 2));
-	if (dist <= obj->radius && proj.x >= obj->coord->x - obj->hei_fov / 2 && proj.x <= obj->coord->x + obj->hei_fov / 2)
+	t_ray	r_cyl;
+	
+	r_cyl = new_ray();
+	r_cyl.direction = obj->vector;
+	r_cyl.origin = *obj->coord;
+
+	t_vec	a;
+	a = point_at(r_cyl, length_vector(r_cyl.direction) / 2);
+
+	
+	
+	ray->direction = unit_vector(ray->direction);
+	abc[0] = length_squared(cross_vector(ray->direction, *obj->vector));
+	abc[1] = 2.0 * scalar_vector(cross_vector(ray->direction, *obj->vector), cross_vector(subtract_vector(ray->origin, *obj->coord), *obj->vector));
+	abc[2] = length_squared(cross_vector(subtract_vector(ray->origin, *obj->coord), *obj->vector)) - pow(obj->radius, 2);
+	t[0] = -abc[1] - sqrt(pow(abc[1], 2) - 4.0 * abc[0] * abc[2]) / (2.0 * abc[0]);
+	t[1] = -abc[1] + sqrt(pow(abc[1], 2) - 4.0 * abc[0] * abc[2]) / (2.0 * abc[0]);
+	if (t[1] < t[0] && t[1] > T_MIN)
+		t[0] = t[1];
+	if (t[0] > T_MIN && t[0] < T_MAX && t[0] < ray->closest_t)
 	{
+		// inter = point_at(*ray, t[0]);
+		inter = add_vector(ray->origin, factor_mult_vector(ray->direction, t[0]));
+		x = sqrt(pow(length_vector(subtract_vector(inter, *obj->coord)) ,2) - pow(obj->radius ,2));
+		// if (x < 30.0)
+			// printf("A_x = %f, height:%f\n", x, obj->hei_fov / 2.0);
+		if (x < (obj->hei_fov / 2.0))
+			return (false);
+		// print_vector(ray->direction);
+		print_vector(inter);
+		printf("\n");
 		ray->closest_object = obj;
-		ray->closest_t = t;
-		ray->normal = *obj->vector;
+		ray->closest_t = t[0];
+		if (scalar_vector(*obj->vector, ray->direction) > 0)
+			ray->normal = subtract_vector(inter, factor_mult_vector(*obj->vector, t[0] * -1.0));
+		else
+			ray->normal = subtract_vector(inter, factor_mult_vector(*obj->vector, t[0]));
 		return (true);
 	}
-	else
-		return (false); // No intersection or intersection not on the lateral surface of the cylinder
+	return (false);
 }
-
-
-//this function renders me a curve
-// bool hit_cylinder(t_ray *ray, t_obj *obj)
-// {
-// 	t_vec	m;
-// 	double	a;
-// 	double	b;
-// 	double	c;
-// 	double	discr;
-// 	double	t1;
-// 	double	t2;
-// 	double	t;
-// 	t_vec	intersection;
-// 	m = subtract_vector(ray->origin, *obj->coord);
-// 	a = scalar_vector(ray->direction, ray->direction) - pow(scalar_vector(ray->direction, *obj->vector), 2);
-// 	b = 2 * (scalar_vector(ray->direction, m) - scalar_vector(ray->direction, *obj->vector) * scalar_vector(m, *obj->vector));
-// 	c = scalar_vector(m, m) - pow(scalar_vector(m, *obj->vector), 2) - pow(obj->radius, 2);
-// 	discr = b * b - 4 * a * c;
-// 	if (discr < 0 || a == 0)
-// 		return (false); // no intersection
-// 	t1 = (-b + sqrt(discr)) / (2 * a);
-// 	t2 = (-b - sqrt(discr)) / (2 * a);
-// 	if (t1 < t2)
-// 		t = t1;
-// 	else
-// 		t = t2;
-// 	intersection = add_vector(ray->origin, factor_mult_vector(ray->direction, t));
-// 	if (intersection.z < obj->coord->z || intersection.z > obj->coord->z + obj->hei_fov)
-// 	    return (false); // intersection is outside cylinder height
-// 	ray->closest_object = obj;
-// 	ray->closest_t = t;
-// 	ray->normal = *obj->vector;
-// 	return (true);
-// }
